09b1d83f5302fd55c02f0474e41422703a5fb58a
MarcoGuglielmino <marco.guglielmino@musement.com>
2023-05-12 18:29:54 +0200

    feat: added footer and link

--- components/LayoutComponent/Footer.vue
+++ components/LayoutComponent/Footer.vue
@@ -1,7 +1,36 @@
 <template>
-  <div></div>
+  <div class="footer">
+    <div class="urls">
+      <p>Made with ❤️ by</p>
+      <a class="name-url" href="https://github.com/marco94gug"
+        >Marco Guglielmino</a
+      >
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import "../../styles/utils";
+.footer {
+  height: 60px;
+  width: 100%;
+  background-color: $mainBlack;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 0.85rem;
+
+  .urls {
+    display: flex;
+    gap: 5px;
+
+    .name-url {
+      color: white;
+      font-weight: bold;
+    }
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -4,8 +4,13 @@
     <main class="main_section">
       <slot />
     </main>
+    <Footer />
   </main>
 </template>
+
+<script lang="ts" setup>
+import Footer from "~/components/LayoutComponent/Footer.vue";
+</script>
 
 <style lang="scss" scoped>
 .main_section {
@@ -15,6 +20,7 @@
 @media only screen and (min-width: 768px) {
   .main_section {
     margin-top: 80px;
+    min-height: calc(100vh - calc(80px + 60px));
   }
 }
 </style>
